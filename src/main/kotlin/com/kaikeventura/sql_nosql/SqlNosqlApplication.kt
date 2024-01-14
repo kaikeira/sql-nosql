33a3f60980516d9a92abb54949449b9917374926
kaikeventura <kaike.gnr@hotmail.com>
2024-01-13 23:11:04 -0300

    Config inicial

---
+++ src/main/kotlin/com/kaikeventura/sql_nosql/SqlNosqlApplication.kt
@@ -0,0 +1,11 @@
+package com.kaikeventura.sql_nosql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SqlNosqlApplication
+
+fun main(args: Array<String>) {
+	runApplication<SqlNosqlApplication>(*args)
+}
---
+++ src/test/kotlin/com/kaikeventura/sql_nosql/SqlNosqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kaikeventura.sql_nosql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SqlNosqlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
